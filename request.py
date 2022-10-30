9d263fecde9098e1d8a9fe75bf998c9a16fcf578
LinqingZ <lingz2oo104@gmail.com>
2022-10-29 20:05:32 -0400

    issues with mechant

---
+++ request.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+customerId = 'your customerId here'
+apiKey = '162961864e9109ece6a8a8d9e297e003'
+
+url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
+payload = {
+  "type": "Savings",
+  "nickname": "test",
+  "rewards": 10000,
+  "balance": 10000,	
+}
+# Create a Savings Account
+response = requests.post( 
+	url, 
+	data=json.dumps(payload),
+	headers={'content-type':'application/json'},
+	)
+
+if response.status_code == 201:
+	print('account created')
